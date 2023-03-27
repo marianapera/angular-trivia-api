227dbd032f2c0917557bc3726eaedc26a6ac7dbc
Mariana Pereira <marianappereira219@gmail.com>
2023-03-27 19:37:45 -0300

    configs: envs e rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/pages/home/home.component';
+import { LayoutDefaultComponent } from './shared/containers/layout-default/layout-default.component';
+import { LayoutDefaultModule } from './shared/containers/layout-default/layout-default.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutDefaultComponent,
+    children: [
+      { path: '', component: HomeComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), LayoutDefaultModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://the-trivia-api.com/api/'
 };
